4503576e318260f315fcdcfa796cc59adb54fa22
tgibbons-css <tgibbons@css.edu>
2017-02-24 10:15:04 -0600

    Initial commit of code for Participation 6

---
+++ app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
@@ -0,0 +1,82 @@
+package css.cis3334.intentsperformances;
+
+import android.content.Intent;
+import android.provider.CalendarContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.GregorianCalendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1Web, but1Calendar, btn1Map;
+    TextView textViewStatus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewStatus = (TextView) findViewById(R.id.textViewStatus);
+        setupButtonClickEvents();
+
+
+
+    }
+
+
+    private void setupButtonClickEvents() {
+        /**
+         *   Set up button click event listener for the web info button for the first performance
+         */
+        btn1Web = (Button) findViewById(R.id.button1WebInfo);
+        btn1Web.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should display website for performance 1");
+            }
+        });
+
+        /**
+         *   Set up button click event listener for the celendar button for the first performance
+         *       Use the calendar intent to set an event reminder on the calendar
+         */
+        but1Calendar = (Button) findViewById(R.id.button1Calendar);
+        but1Calendar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should set a calendar event for performance 1");
+                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setType("vnd.android.cursor.item/event");
+                calIntent.putExtra(CalendarContract.Events.TITLE, "SAL Colloquium 'The Robot Next Door'");
+                calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Tower Hall 4119");
+                GregorianCalendar begDate = new GregorianCalendar(2017, 2, 24, 3, 40);
+                GregorianCalendar endDate = new GregorianCalendar(2017, 2, 24, 4, 40);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        begDate.getTimeInMillis());
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                        endDate.getTimeInMillis());
+                startActivity(calIntent);
+
+            }
+        });
+
+        /**
+         *   Set up button click event listener for the web info button for the first performance
+         */
+        btn1Map = (Button) findViewById(R.id.button1map);
+        btn1Map.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should display map for performance 1");
+            }
+        });
+
+
+    }
+
+
+}
